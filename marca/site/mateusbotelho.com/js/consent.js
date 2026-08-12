54acf5bb6e9e0c0ce1d86bc803ce351504fa3430
Mateus Botelho <mateusbotelhopro@gmail.com>
2026-08-12 00:33:45 -0300

    auto-sync: 2026-08-12 00:33

--- marca/site/mateusbotelho.com/js/consent.js
+++ marca/site/mateusbotelho.com/js/consent.js
@@ -6,6 +6,8 @@
     var GTM_ID = 'GTM-PFLSM4T8';
     var PIXEL_ID = '840015548656031';
     var CS_SRC = 'https://t.contentsquare.net/uxa/af7aa2159c920.js';
+    var AW_ID = 'AW-17962154967';
+    var AW_LABEL_CONTATO = 'iBaWCPH4mOAcENf3gvVC';
 
     // Google Tag Manager
     (function (w, d, s, l, i) {
@@ -15,6 +17,27 @@
         j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
         f.parentNode.insertBefore(j, f);
     })(window, document, 'script', 'dataLayer', GTM_ID);
+
+    // Google Ads (conversão: clique nos links de WhatsApp)
+    (function () {
+        var s = document.createElement('script');
+        s.async = true;
+        s.src = 'https://www.googletagmanager.com/gtag/js?id=' + AW_ID;
+        document.head.appendChild(s);
+    })();
+    window.dataLayer = window.dataLayer || [];
+    function gtag() { dataLayer.push(arguments); }
+    gtag('js', new Date());
+    gtag('config', AW_ID);
+    document.querySelectorAll('a[href*="wa.me"]').forEach(function (link) {
+        link.addEventListener('click', function () {
+            gtag('event', 'conversion', {
+                'send_to': AW_ID + '/' + AW_LABEL_CONTATO,
+                'value': 1.0,
+                'currency': 'BRL'
+            });
+        });
+    });
 
     // Meta Pixel
     !function (f, b, e, v, n, t, s) {
